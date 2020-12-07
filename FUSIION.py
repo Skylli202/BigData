916a5d8f11b6a21b71693424c1f0e54b941a2929
Skylli <elouangouinguenet@yahoo.fr>
2020-12-07 16:14:57 +0100

    working great !

--- FUSIION.py
+++ FUSIION.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.datasets.samples_generator import make_blobs
+from sklearn.preprocessing import StandardScaler
+
 # from sklearn.cluster import KMeans
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
@@ -9,8 +11,10 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.clustering import KMeans
 from pyspark.ml.evaluation import ClusteringEvaluator
 
+
 def strToBool(str):
     return str.lower() in ("yes")
+
 
 def init_spark():
     spark = SparkSession \
@@ -20,11 +24,13 @@ def init_spark():
     sc = spark.sparkContext
     return spark, sc
 
+
 spark, sc = init_spark()
 df = spark.read.option("delimiter", ";").csv(
     # "bank.csv", header=True,
     "bank-mini.csv", header=True,
 )
+print("Schema after reading CSV file :")
 df.printSchema()
 
 # Change column type
@@ -35,12 +41,13 @@ df2 = df2.withColumn('campaign', df['campaign'].cast("int"))
 df2 = df2.withColumn('pdays', df['pdays'].cast("int"))
 df2 = df2.withColumn('previous', df['previous'].cast("int"))
 
-# Remplace
-df2 = df2.withColumn('default',F.when(F.col('default')=="yes", 1).otherwise(0))
-df2 = df2.withColumn('housing',F.when(F.col('housing')=="yes", 1).otherwise(0))
-df2 = df2.withColumn('loan',F.when(F.col('loan')=="yes", 1).otherwise(0))
-df2 = df2.withColumn('y',F.when(F.col('y')=="yes", 1).otherwise(0))
-df2 = df2.withColumn('contact',F.when(F.col('contact')=="cellular", 0).when(F.col('contact')=="unknown", 0.5).otherwise(1))
+# Replace text by integer value
+df2 = df2.withColumn('default', F.when(F.col('default') == "yes", 1).otherwise(0))
+df2 = df2.withColumn('housing', F.when(F.col('housing') == "yes", 1).otherwise(0))
+df2 = df2.withColumn('loan', F.when(F.col('loan') == "yes", 1).otherwise(0))
+df2 = df2.withColumn('y', F.when(F.col('y') == "yes", 1).otherwise(0))
+df2 = df2.withColumn('contact',
+                     F.when(F.col('contact') == "cellular", 0).when(F.col('contact') == "unknown", 0.5).otherwise(1))
 
 # Drop column that we don't care
 df2 = df2.drop(df2.job)
@@ -50,22 +57,34 @@ df2 = df2.drop(df2.day)
 df2 = df2.drop(df2.month)
 df2 = df2.drop(df2.poutcome)
 
-df2.printSchema()
-FEATURES_COL = ['age', 'default', 'balance', 'housing', 'loan', 'contact', 'duration', 'campaign', 'pdays', 'previous', 'y']
+FEATURES_COL = ['age',
+                'default',
+                'balance',
+                'housing',
+                'loan',
+                'contact',
+                'duration',
+                'campaign',
+                'pdays',
+                'previous',
+                'y']
 
 for col in df2.columns:
     if col in FEATURES_COL:
-        df2 = df2.withColumn(col,df2[col].cast('float'))
-# df2.show()
+        df2 = df2.withColumn(col, df2[col].cast('float'))
 
 df2 = df2.na.drop()
-# df2.show()
+
+print("Schema after pre-process for KMeans:")
+df2.printSchema()
 
 vecAssembler = VectorAssembler(inputCols=FEATURES_COL, outputCol="features")
 df_kmeans = vecAssembler.transform(df2).select('features')
+
+print("KMeans dataframe :")
 df_kmeans.show(20, False)
 
-# Evaluation de K
+# # Evaluation de K (to figure out which value of K we need to use)
 # cost = np.zeros(20)
 # for k in range(2,20):
 #     kmeans = KMeans().setK(k).setSeed(1).setFeaturesCol("features");
@@ -85,48 +104,58 @@ df_kmeans.show(20, False)
 # ax.set_ylabel('cost')
 # fig.show()
 
-# Détection du centre des cluster
+# KMeans evaluation of Cluster's center location
 k = 3
 kmeans = KMeans().setK(k).setSeed(1).setFeaturesCol("features")
 model = kmeans.fit(df_kmeans)
 centers = model.clusterCenters()
 
-print("Cluster Centers: ")
-for center in centers:
-    print(center)
+# # Print center position
+# print("Cluster Centers: ")
+# for center in centers:
+#     print(center)
 
+# Now, add the prediction column to our df2
 transformed = model.transform(df_kmeans).select('prediction')
 rows = transformed.collect()
-print(rows)
 
 df_pred = spark.createDataFrame(rows)
-df_pred.show()
-
 df_pred = df_pred.join(df2)
-df_pred.show()
 
-# enfaite on veut bien une colonne ID
+# We need an ID column, so here it is...
 df_pred = df_pred.withColumn("id", F.monotonically_increasing_id())
+
+print("Dataframe with predict, features and ID")
 df_pred.show()
 
 pddf_pred = df_pred.toPandas().set_index('id')
-pddf_pred.head()
 
-threedee = plt.figure(figsize=(12,10)).gca(projection='3d')
-threedee.scatter(pddf_pred.age, pddf_pred.balance, pddf_pred.duration,  c=pddf_pred.prediction)
-threedee.set_xlabel('age')
-threedee.set_ylabel('balance')
-threedee.set_zlabel('duration')
+print("pandas df:")
+print(pddf_pred.head())
+
+# PCA
+print("\n\n---------------- PCA ----------------")
+from sklearn.decomposition import PCA
+
+pddf_numpy = pddf_pred.to_numpy()
+print(pddf_numpy.shape, pddf_numpy)
+
+pca = PCA(n_components=2)
+pca.fit(pddf_numpy)
+pddf_numpy_pca = pca.transform(pddf_numpy)
+pddf_numpy_pca_inverse = pca.inverse_transform(pddf_numpy_pca)
+print("original shape:   ", pddf_numpy.shape)
+print("transformed shape:", pddf_numpy_pca.shape)
+print("inverse transformed shape:", pddf_numpy_pca_inverse.shape)
+
+# plt it !
+# threedee = plt.figure(figsize=(12,10)).gca(projection='3d')
+
+plt.scatter(pddf_numpy_pca[:, 0], pddf_numpy_pca[:, 1])
+# threedee.set_xlabel('age')
+# threedee.set_ylabel('balance')
+# threedee.set_zlabel('duration')
 plt.show()
 
+print("\n\n\n\n\nsc.stop() :")
 sc.stop()
-# ----------------------------------------
-#                K-means
-# ----------------------------------------
-
-
-# kmeans = KMeans(n_clusters=4, init='k-means++', max_iter=300, n_init=10, random_state=0)
-# pred_y = kmeans.fit_predict(df_kmeans)
-# plt.scatter(df_kmeans[:,0], df_kmeans[:,1])
-# plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='red')
-# plt.show()
